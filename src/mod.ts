cb362523e414657798d29c491b3283c3aa605faa
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-12-17 12:00:40 +0000

    fix: re-export

--- src/mod.ts
+++ src/mod.ts
@@ -7,6 +7,8 @@
  */
 
 export * from './arraybuffer/mod.ts'
+export * from './base64/mod.ts'
+export * from './bigint/mod.ts'
 export * from './blob/mod.ts'
 export * from './hex/mod.ts'
 export * from './uint8array/mod.ts'
